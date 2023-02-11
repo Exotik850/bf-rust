3155a60978bd47e25ec1fe287a72c159bfa1e995
Exotik850 <kidkool850@gmail.com>
2023-02-11 16:46:49 -0600

    Fixins

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,29 @@
 use std::collections::HashMap;
-use std::io::Write;
+use std::io::{Read, Write};
 use std::num::Wrapping;
-use std::ops::Deref;
+use std::ops::{AddAssign, Deref};
 use std::time::SystemTime;
 
 const MAX_ITER: u64 = 1000000000;
 
-// The BfToken enum represents the possible operations in Brainfuck language.
+// Represents the possible operations in Brainf*** language.
 #[derive(Debug, Copy, Clone)]
 enum BfToken {
-    INC(usize), // Increment the current cell by N
-    DEC(usize), // Decrement the current cell by N
-    LEF(usize), // Move the pointer to the left by N
-    RIG(usize), // Move the pointer to the right by N
-    JUM,  // Jump if the value of the current cell is zero
-    BAC,  // Jump to the matching opening bracket
-    ACC,  // Accept one byte of input, storing its value in the current cell
-    OUT,  // Output the value of the current cell as a character
-    NAN,  // Not a valid operation
+    CEL(isize), // Increment the current cell by N
+    MOV(isize), // Move the pointer by N
+    JUM,        // Jump if the value of the current cell is zero
+    BAC,        // Jump to the matching opening bracket
+    ACC,        // Accept one byte of input, storing its value in the current cell
+    OUT,        // Output the value of the current cell as a character
+    NAN,        // Not a valid operation
 }
 
 impl PartialEq for BfToken {
     fn eq(&self, other: &Self) -> bool {
         match (self, other) {
             // Match against the same variant for each operation
-            (Self::INC(_), Self::INC(_)) => true,
-            (Self::DEC(_), Self::DEC(_)) => true,
-            (Self::LEF(_), Self::LEF(_)) => true,
-            (Self::RIG(_), Self::RIG(_)) => true,
-            (Self::JUM, Self::JUM) => true,
-            (Self::BAC, Self::BAC) => true,
-            (Self::ACC, Self::ACC) => true,
-            (Self::OUT, Self::OUT) => true,
-            (Self::NAN, Self::NAN) => true,
+            (Self::CEL(_), Self::CEL(_)) => true,
+            (Self::MOV(_), Self::MOV(_)) => true,
             (_, _) => false,
         }
     }
@@ -42,15 +33,25 @@ impl PartialEq for BfToken {
 impl From<char> for BfToken {
     fn from(value: char) -> Self {
         match value {
-            '>' => Self::RIG(1),
-            '<' => Self::LEF(1),
-            '+' => Self::INC(1),
-            '-' => Self::DEC(1),
+            '>' => Self::MOV(1),
+            '<' => Self::MOV(-1),
+            '+' => Self::CEL(1),
+            '-' => Self::CEL(-1),
             '.' => Self::OUT,
             ',' => Self::ACC,
             '[' => Self::JUM,
             ']' => Self::BAC,
             _ => Self::NAN,
+        }
+    }
+}
+
+impl AddAssign for BfToken {
+    fn add_assign(&mut self, rhs: Self) {
+        match (self, &rhs) {
+            (Self::CEL(N), Self::CEL(A)) => *N += A,
+            (Self::MOV(N), Self::MOV(A)) => *N += A,
+            (_, _) => (),
         }
     }
 }
@@ -67,29 +68,26 @@ impl BfToken {
                 T => Some(T),
             })
             .collect();
+
         // Combine successive instances of the same operation into a single instance with the sum of their values.
         tokens = tokens.iter().fold(vec![BfToken::NAN], |mut acc, next| {
             let last = acc.len() - 1;
-            match (acc.last().unwrap(), next) {
-                (BfToken::INC(N), BfToken::INC(M)) => acc[last] = BfToken::INC(N + M),
-                (BfToken::LEF(N), BfToken::LEF(M)) => acc[last] = BfToken::LEF(N + M),
-                (BfToken::RIG(N), BfToken::RIG(M)) => acc[last] = BfToken::RIG(N + M),
-                (BfToken::DEC(N), BfToken::DEC(M)) => acc[last] = BfToken::DEC(N + M),
-                (_, &curr) => acc.push(curr),
+            if acc[last].eq(next) {
+                acc[last] += *next;
+            } else {
+                acc.push(*next);
             }
             acc
         });
 
         // Create a map of the jumps for the bracket commands
-        let mut jumps = vec![0;code.len()];
+        let mut jumps = vec![0; code.len()];
         let mut queue = vec![];
         for (idx, token) in tokens.iter().enumerate() {
             match token {
                 BfToken::JUM => queue.push(idx),
                 BfToken::BAC => {
-                    let temp = queue
-                        .pop()
-                        .expect(&format!("Unopened bracket at {idx}"));
+                    let temp = queue.pop().expect(&format!("Unopened bracket at {idx}"));
 
                     // Write the jump destination to the index of the token
                     jumps[temp] = idx;
@@ -116,7 +114,7 @@ fn parse(code: &str) {
     // Initialize "system" variables
     let mut stack = vec![0u8];
     let (tokens, jumps) = BfToken::from_source(code);
-    let mut pointer = 0;
+    let mut pointer = 0usize;
     let mut idx = 0;
 
     // If there is an input token, convert an input string to it's bytes
@@ -137,25 +135,41 @@ fn parse(code: &str) {
     let mut iter = 0u64; // For optional iteration cap
     while idx < tokens.len() {
         match tokens[idx] {
-            BfToken::RIG(N) => {
-                // Check if there is room on the stack to move right, if not make room
-                if pointer + N >= stack.len() {
-                    stack.extend(vec![0; N]);
-                }
-                pointer += N;
-            }
-            BfToken::LEF(N) => {
-                // Opposite for moving left
-                if pointer >= N {
-                    pointer -= N
+            BfToken::MOV(N) => {
+                if N > 0 {
+                    let n = N as usize;
+                    // Check if there is room on the stack to move right, if not make room
+                    if pointer + n >= stack.len() {
+                        stack.extend(vec![0; n]);
+                    }
+                    pointer += n;
                 } else {
-                    stack.splice(0..0, vec![0u8; N]);
+                    // Opposite for moving left
+                    let n = N.abs() as usize;
+                    if pointer >= n {
+                        pointer -= n
+                    } else {
+                        stack.splice(0..0, vec![0; n - pointer]);
+                    }
                 }
             }
-            BfToken::INC(N) => stack[pointer] = stack[pointer].wrapping_add(N as u8),
-            BfToken::DEC(N) => stack[pointer] = stack[pointer].wrapping_sub(N as u8),
-            BfToken::JUM => if stack[pointer] == 0 { idx = jumps[idx] },
-            BfToken::BAC => if stack[pointer] != 0 { idx = jumps[idx] },
+            BfToken::CEL(N) => {
+                if N > 0 {
+                    stack[pointer] = stack[pointer].wrapping_add(N as u8);
+                } else {
+                    stack[pointer] = stack[pointer].wrapping_sub((-N) as u8);
+                }
+            }
+            BfToken::JUM => {
+                if stack[pointer] == 0 {
+                    idx = jumps[idx]
+                }
+            }
+            BfToken::BAC => {
+                if stack[pointer] != 0 {
+                    idx = jumps[idx]
+                }
+            }
             BfToken::ACC => stack[pointer] = input.pop().unwrap_or(0),
             BfToken::OUT => out.push(stack[pointer] as char),
             _ => (),
@@ -163,13 +177,11 @@ fn parse(code: &str) {
         idx += 1;
         iter += 1;
     }
-
+    let time = SystemTime::now().duration_since(start).unwrap();
     println!("{:?}", stack);
     println!("{}", out.iter().collect::<String>());
-    println!(
-        "Time taken: {:?}\nIterations: {iter}",
-        SystemTime::now().duration_since(start).unwrap()
-    )
+    println!("Time taken: {time:?}\nCommands Processed: {iter}");
+    // println!("{tokens:?}");
 }
 
 fn main() {
